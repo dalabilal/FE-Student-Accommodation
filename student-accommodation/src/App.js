8d6c75bb02ad44f38e3f49cf919a5ce06ba04a4b
dala_bila <dalahhashlamoon@gmail.com>
2023-12-04 14:43:18 +0200

    add routers

--- student-accommodation/src/App.js
+++ student-accommodation/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import SignInForm from './pages/login/login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
  return (
-    
-     <SignInForm/>
- 
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signin' element={<SignInForm/>}/>
+      </Routes>
+    </BrowserRouter>
+
  );
 }
 

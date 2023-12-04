e1f667c7f2048cd9291439b80b1c84b7f9caf24e
dala_bila <dalahhashlamoon@gmail.com>
2023-12-04 14:37:41 +0200

    some css

--- student-accommodation/src/App.js
+++ student-accommodation/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 import SignInForm from './pages/login/login';
 
 function App() {
  return (
-    <div className="App">
+    
      <SignInForm/>
-    </div>
+ 
  );
 }
 

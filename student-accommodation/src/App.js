953899ca2379339b9fedbd4c133cbafab1de403f
dala_bila <dalahhashlamoon@gmail.com>
2023-12-04 15:49:04 +0200

    add multiple pages

--- student-accommodation/src/App.js
+++ student-accommodation/src/App.js
@@ -1,15 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AllAccomodation from './pages/All-accomodation/AllAccomodation';
+import Favorite from './pages/Favorite/Favorite';
 import HomePage from './pages/HomePage/homePage';
 import SignInForm from './pages/login/login';
 import SignUp from './pages/sign-up/sign-up';
+import Users from './pages/AllUsers/Users';
+import NotFound from './pages/Not-found/NotFound';
+import Header from './component/Logo/Header';
 
 function App() {
  return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/signin' element={<SignInForm/>}/>
         <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/all' element={<AllAccomodation/>}/>
+        <Route path='/favorite' element={<Favorite/>}/>
+        <Route path='/allusers' element={<Users/>}/>
+        <Route path='/*' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
 
---
+++ student-accommodation/src/component/Logo/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      Header
+    </div>
+  )
+}
+
+export default Header

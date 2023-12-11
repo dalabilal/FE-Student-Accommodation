4e6a7ffc4338e4e68e2758cfbcc448c391a54536
dala_bila <dalahhashlamoon@gmail.com>
2023-12-11 13:12:40 +0200

    create navbar component

--- student-accommodation/src/App.js
+++ student-accommodation/src/App.js
@@ -6,12 +6,12 @@ import SignInForm from './pages/login/login';
 import SignUp from './pages/sign-up/sign-up';
 import Users from './pages/AllUsers/Users';
 import NotFound from './pages/Not-found/NotFound';
-import Header from './component/Logo/Header';
+import NavBar from './component/common/navBar/NavBar';
 
 function App() {
  return (
     <BrowserRouter>
-    <Header/>
+    <NavBar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/signin' element={<SignInForm/>}/>

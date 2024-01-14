f146f380798927d156d3f75732d5a07e45205a0c
Shatha-Awawdh <227044@ppu.edu.ps>
2024-01-14 21:23:59 +0200

    navbarFirst

--- student-accommodation/src/component/common/navBar/NavBar.jsx
+++ student-accommodation/src/component/common/navBar/NavBar.jsx
@@ -1,11 +1,29 @@
-import './navBar.css'
+import "./navBar.css";
+import React from "react";
+import { HouseLine, UserCircle } from "@phosphor-icons/react";
+
 
 const NavBar = () => {
   return (
-    <div className='container'>
-      
+    <div className="container">
+      <HouseLine id="houseLine" size={32} />
+      <h1 id="sh housing">Hebron Student Housing</h1>
+      <ul>
+        <li id='allAccommodations'>
+          <a href="allAccommodations">All Accommodations</a>
+        </li>
+        <li id="Favorite">
+          <a href="Favorite">Favorite</a>
+        </li>
+        <li id="Home">
+          <a href="Home">Home</a>
+        </li>
+        <li id="UserCircle">
+          <UserCircle size={32} />
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;

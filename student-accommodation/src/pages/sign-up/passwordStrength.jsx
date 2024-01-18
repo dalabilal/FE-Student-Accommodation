9ddbaf255370c17795c0ea1e1576754e04137e35
dala_bila <dalahhashlamoon@gmail.com>
2024-01-18 23:24:16 +0200

    confirm password

--- student-accommodation/src/pages/sign-up/passwordStrength.jsx
+++ student-accommodation/src/pages/sign-up/passwordStrength.jsx
@@ -7,11 +7,12 @@ import zxcvbn from 'zxcvbn';
 
 const StrongPassword = () => {
   const [password, setPassword] = useState('');
+  const [password1, setPassword1] = useState('');
   const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
 
   const meetsPasswordCriteria = () => {
-    // Password criteria: at least 8 characters, contain at least one uppercase and one lowercase character,
-    // contain a digit, contain a special character
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
     return regex.test(password);
   };
@@ -40,6 +41,14 @@ const StrongPassword = () => {
     return '#FFAD00'; // Orange for medium passwords
   };
 
+  const handlePassword1Change = (e) => {
+    const confirmPassword = e.target.value;
+    setPassword1(confirmPassword);
+
+    // Check if passwords match
+    setPasswordsMatch(confirmPassword === password);
+  };
+
   return (
     <div className="container1">
       <div className="col-md-6 mx-auto text-right">
@@ -60,6 +69,18 @@ const StrongPassword = () => {
             color={getPasswordStrengthColor()}
           />
         )}
+        <div className="form-group mb-1">
+          <Input
+            className={`form-control shadow-none ${!passwordsMatch ? 'password-mismatch' : ''}`}
+            label="Confirm Password"
+            onChange={handlePassword1Change}
+            type={show1 ? 'text' : 'password'}
+          />
+          <span style={{ color: '#A3C195' }} onClick={() => setShow1(!show1)}>
+            {show1 ? <Eye size={30} color="black" /> : <EyeClosed size={30} color="black" />}
+          </span>
+          {!passwordsMatch && <p style={{ color: 'red' }}>Passwords do not match!</p>}
+        </div>
       </div>
     </div>
   );

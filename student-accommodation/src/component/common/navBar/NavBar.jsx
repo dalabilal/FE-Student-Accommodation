681a9d21b4e7d605dd0a061326e534c63180b857
Shatha-Awawdh <227044@ppu.edu.ps>
2024-01-20 00:17:58 +0200

    navBar,searchBar

--- student-accommodation/src/component/common/navBar/NavBar.jsx
+++ student-accommodation/src/component/common/navBar/NavBar.jsx
@@ -4,43 +4,75 @@ import { HouseLine, UserCircle } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const [isListVisible, setListVisible] = useState(false);
+  const [Options, setOptions] = useState(false);
+  const [activeItem, setActiveItem] = useState(null);
 
-  const toggleList = () => {
-    setListVisible(!isListVisible);
+  const handleOptions = () => {
+    setOptions(false);
+  };
+
+  const handleItemClick = (itemName) => {
+    setActiveItem(itemName);
+    setOptions(false);
   };
 
   return (
-    <>
+    <div className="navBarWithList">
       <div className="container">
         <HouseLine id="houseLine" size={32} weight="bold" />
         <h1 id="HShousing">Hebron Student Housing</h1>
-        <ul>
-          <li id="allAccommodations">
+        <ul className="vanBarOptions">
+          <li
+            className={activeItem === "Accommodations" ? "active" : ""}
+            onClick={() => handleItemClick("Accommodations")}
+          >
             <Link to="all">Accommodations</Link>
           </li>
-          <li id="Favorite">
+          <li
+            className={activeItem === "Favorite" ? "active" : ""}
+            onClick={() => handleItemClick("Favorite")}
+          >
             <Link to="Favorite">Favorite</Link>
           </li>
-          <li id="Home">
+          <li
+            className={activeItem === "Home" ? "active" : ""}
+            onClick={() => handleItemClick("Home")}
+          >
             <Link to="/">Home</Link>
           </li>
-          <li id="Home">
+          <li
+            className={activeItem === "Users" ? "active" : ""}
+            onClick={() => handleItemClick("Users")}
+          >
             <Link to="users">Users</Link>
           </li>
         </ul>
 
         <div id="UserCircle">
-          <UserCircle size={32} onClick={toggleList} />
+          <UserCircle size={32} onClick={() => setOptions(!Options)} />
         </div>
-
       </div>
 
-      <ul className={`nav-list ${isListVisible ? "visible" : ""}`}>
-        <li>Login</li>
-        <li>Sign Up</li>
-      </ul>
-    </>
+      {Options && (
+        <ul className="options">
+          <li onClick={handleOptions}>
+            <Link id="signin" to="signin">
+              Sign In
+            </Link>
+          </li>
+          <li onClick={handleOptions}>
+            <Link id="signup" to="signup">
+              Sign Up
+            </Link>
+          </li>
+          <li onClick={handleOptions}>
+            <Link id="LogOut" to="">
+              Log Out
+            </Link>
+          </li>
+        </ul>
+      )}
+    </div>
   );
 };
 

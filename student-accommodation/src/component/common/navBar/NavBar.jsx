50cdec3b93aedbc51d3c25c326da3cb73ea9bcb8
dala_bila <dalahhashlamoon@gmail.com>
2024-01-19 20:10:45 +0200

    logout

--- student-accommodation/src/component/common/navBar/NavBar.jsx
+++ student-accommodation/src/component/common/navBar/NavBar.jsx
@@ -1,10 +1,32 @@
 import "./navBar.css";
 import React, { useState } from "react";
 import { HouseLine, UserCircle } from "@phosphor-icons/react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useUser } from "../../../service/UserContext";
 
 const NavBar = () => {
   const [isListVisible, setListVisible] = useState(false);
+  const { setUserRole, userRole } = useUser();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('http://localhost:3005/logout/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        setUserRole(null);
+        navigate('/signin');
+      } else {
+        console.error('Logout failed');
+      }
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  };
 
   const toggleList = () => {
     setListVisible(!isListVisible);
@@ -33,12 +55,19 @@ const NavBar = () => {
           <UserCircle size={32} onClick={toggleList} />
         </div>
 
+        {userRole ? (
+          <button onClick={handleLogout}>Logout</button>
+        ) : (
+          <ul className={`nav-list ${isListVisible ? "visible" : ""}`}>
+            <li>
+              <Link to="/signin">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </ul>
+        )}
       </div>
-
-      <ul className={`nav-list ${isListVisible ? "visible" : ""}`}>
-        <li>Login</li>
-        <li>Sign Up</li>
-      </ul>
     </>
   );
 };

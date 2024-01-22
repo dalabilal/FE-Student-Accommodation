ca51a40acaec2960d75ba5392d10ba9447bd0f18
dala_bila <dalahhashlamoon@gmail.com>
2024-01-22 22:06:13 +0200

    create user profile page

--- student-accommodation/src/App.js
+++ student-accommodation/src/App.js
@@ -2,8 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { UserProvider } from './service/UserContext';
 
 import AllAccomodation from './pages/All-accomodation/AllAccomodation';
-import ProtectedUsers from './pages/protected/ProtectedUsers';
 import Notification from './notification/notification.component';
+import ProtectedUsers from './pages/protected/ProtectedUsers';
+import Profile from './pages/user-profile/Profile';
 import Favorite from './pages/Favorite/Favorite';
 import NotFound from './pages/Not-found/NotFound';
 import HomePage from './pages/HomePage/homePage';
@@ -22,6 +23,7 @@ function App() {
         <Route path='/' element={<HomePage/>}/>
         <Route path='/signin' element={<SignInForm/>}/>
         <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
         <Route path='/all' element={<AllAccomodation/>}/>
         <Route path='/favorite' element={<Favorite/>}/>
         <Route path='/allusers' element={<Guard permittedRoles={['owner']}><ProtectedUsers /></Guard>} />

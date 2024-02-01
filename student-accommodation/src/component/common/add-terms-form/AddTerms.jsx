9b904c94135151697c05d9aacb92e9cbd03c82c5
dala_bila <dalahhashlamoon@gmail.com>
2024-02-01 17:56:04 +0200

    create add terms button

---
+++ student-accommodation/src/component/common/add-terms-form/AddTerms.jsx
@@ -0,0 +1,89 @@
+import Input from "../input/input.component";
+import Textarea from "../textarea/textarea.component";
+import sanitizeHtml from 'sanitize-html';
+import "./addterms.css";
+import { useUser } from "../../../service/UserContext";
+
+const AddTerms = (props) => {
+  const {idparam } = useUser();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const sanitizeOptions = {
+      allowedTags: ['b', 'i', 'em', 'strong', 'a', 'p', 'br'],
+      allowedAttributes: {
+        'a': ['href'],
+      },
+      transformTags: {
+        'script': function (tagName, attribs) {
+          alert('Script tag not allowed!');
+          return { tagName: 'div', text: 'Script tag not allowed!' };
+        },
+      },
+    };
+
+    const fees = sanitizeHtml(e.target.fee.value, sanitizeOptions);
+    const term = sanitizeHtml(e.target.terms.value, sanitizeOptions);
+    const useID = sessionStorage.getItem('userID');
+
+    const termData = {
+      fees : fees,
+      term :term,
+      housingId :idparam,
+      ownerId : useID
+    };
+
+    try {
+      const response = await fetch("http://localhost:3005/term/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(termData),
+      });
+
+      if (response.ok) {
+        console.log("terms submitted successfully");
+        props.setPopup(false);
+      } else {
+        console.error("Failed to add terms");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div className="plur-popup">
+      <div className="popup-container show">
+        <div className="xDiv">
+          <h2>Add Your Terms details</h2>
+          <span id="x" onClick={() => props.setPopup(false)}>
+            X
+          </span>
+        </div>
+        <form onSubmit={handleSubmit} encType="multipart/formData">
+          <Input
+            label="fees Per month $"
+            type="number"
+            name="fee"
+            required
+          />
+          <Textarea
+            label="provide your  terms "
+            name="terms"
+            required
+          />
+          <div className="bottuns">
+            <button id="add" type="submit">
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddTerms;
--- student-accommodation/src/pages/view-details-housing/view-housing.jsx
+++ student-accommodation/src/pages/view-details-housing/view-housing.jsx
@@ -3,62 +3,81 @@ import { useNavigate, useParams } from 'react-router-dom';
 import useNotification from '../../hook/notification.hook';
 import './view-housing.css'
 import { useUser } from '../../service/UserContext';
+import AddTerms from '../../component/common/add-terms-form/AddTerms';
 
-const imageUrl="https://th.bing.com/th/id/OIP.OfQ9D-ht_ihNi9sbI7mZlwHaEK?rs=1&pid=ImgDetMain"
+const imageUrl = "https://th.bing.com/th/id/OIP.OfQ9D-ht_ihNi9sbI7mZlwHaEK?rs=1&pid=ImgDetMain"
 
 const ViewHousing = () => {
-    const { id } = useParams();
-    const [housingData, setHousingData] = useState(null);
-    const { setNotification } = useNotification();
-    const username = sessionStorage.getItem('username');
-    const navigate = useNavigate();
-    const {setIdparam} = useUser();
-    
-    useEffect(() => {
-        const fetchHousingData = async () => {
-          try {
-            const response = await fetch(`http://localhost:3005/all/housing/${id}`);
-            if (response.ok) {
-              const data = await response.json();
-              setIdparam(id);
-              setHousingData(data);
-            } else {
-              console.error('Failed to fetch housing data:', response.statusText);
-              setNotification({ message: 'Failed to fetch housing data', status: 'err' });
-            }
-          } catch (error) {
-            console.error('Error during fetch:', error.message);
-            setNotification({ message: 'Error during fetch', status: 'err' });
-          }
-        };
-    
-        fetchHousingData();
-      }, [id]);
-    
-      if (!housingData) {
-        return <div>Loading...</div>;
+  const { id } = useParams();
+  const [housingData, setHousingData] = useState(null);
+  const [addTerms, setAddTerms] = useState(false);
+  const { setNotification } = useNotification();
+  const username = sessionStorage.getItem('username');
+  const navigate = useNavigate();
+  const { setIdparam, userRole } = useUser();
+
+  useEffect(() => {
+    const fetchHousingData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3005/all/housing/${id}`);
+        if (response.ok) {
+          const data = await response.json();
+          setIdparam(id);
+          setHousingData(data);
+        } else {
+          console.error('Failed to fetch housing data:', response.statusText);
+          setNotification({ message: 'Failed to fetch housing data', status: 'err' });
+        }
+      } catch (error) {
+        console.error('Error during fetch:', error.message);
+        setNotification({ message: 'Error during fetch', status: 'err' });
       }
+    };
+
+    fetchHousingData();
+  }, [id]);
+
+  if (!housingData) {
+    return <div>Loading...</div>;
+  }
 
 
   return (
     <div className='view-page-group-buttons'>
-    <div className="view-page-group">
-            <img className="image-housing" src={imageUrl} alt='housing pic'/>
+      <div className="view-page-group">
+        <img className="image-housing" src={imageUrl} alt='housing pic' />
         <div className="information">
-                   <h2>{housingData.name}</h2>
-                   <p id='cardItem'>{housingData.description}</p>
-                   <p id='cardItem'>Number of room : {housingData.rooms}</p>
-                   <p id='cardItem'>{housingData.location}</p>
-                   <p id='cardItem'>Added by : {username}</p>
-                   <p id='cardItem'>{housingData.phoneNumber}</p>
+          <h2>{housingData.name}</h2>
+          <p id='cardItem'>{housingData.description}</p>
+          <p id='cardItem'>Number of room : {housingData.rooms}</p>
+          <p id='cardItem'>{housingData.location}</p>
+          <p id='cardItem'>Added by : {username}</p>
+          <p id='cardItem'>{housingData.phoneNumber}</p>
         </div>
-        <div className="payment">
-          <button onClick={() =>navigate('/payment')}>Book now</button>
-        </div>
-    </div>
-    <div className="buttons-container">
-        <button id='cardB'>Show Rental terms</button>
-        <button id='cardB'>Book Now!</button>
+      </div>
+      <div className="buttons-container">
+       {userRole === 'student' ?
+          <>
+            <button
+              id='cardB'
+        >
+              Show Rental terms
+            </button>
+            <button
+              onClick={() => navigate('/payment')}
+              id='cardB'
+            >
+              Book Now!
+            </button>
+            </> :
+           <button onClick={() => setAddTerms(true)}>Add rental terms</button>
+       }
+       {
+        addTerms &&
+         <AddTerms
+         setPopup={setAddTerms}
+        />
+       }
       </div>
     </div>
   )
